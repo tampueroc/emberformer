6f883ab56057617e3da4ff2d5f86a75805239b22
Tom Ampuero <tom.ampuero.c@gmail.com>
2025-10-31 19:15:52 +0000

    biz

--- scripts/viz_report.py
+++ scripts/viz_report.py
@@ -263,15 +263,15 @@ class USpaceVisualizer:
         fig = plt.figure(figsize=(14, 12))
         ax = fig.add_subplot(111, projection='3d')
         
-        # Plot convex hull surface (swap axes: U3=x, U2=y, U1=z)
+        # Plot convex hull surface (swap axes: -U3=x, U2=y, U1=z)
         if hull is not None:
-            # Get the vertices for each simplex with swapped axes
+            # Get the vertices for each simplex with swapped axes (flip U3)
             verts = []
             for simplex in hull.simplices:
-                # Original: [U1, U2, U3], swap to [U3, U2, U1]
-                v0 = [U_hull[simplex[0], 2], U_hull[simplex[0], 1], U_hull[simplex[0], 0]]
-                v1 = [U_hull[simplex[1], 2], U_hull[simplex[1], 1], U_hull[simplex[1], 0]]
-                v2 = [U_hull[simplex[2], 2], U_hull[simplex[2], 1], U_hull[simplex[2], 0]]
+                # Original: [U1, U2, U3], swap to [-U3, U2, U1]
+                v0 = [-U_hull[simplex[0], 2], U_hull[simplex[0], 1], U_hull[simplex[0], 0]]
+                v1 = [-U_hull[simplex[1], 2], U_hull[simplex[1], 1], U_hull[simplex[1], 0]]
+                v2 = [-U_hull[simplex[2], 2], U_hull[simplex[2], 1], U_hull[simplex[2], 0]]
                 verts.append([v0, v1, v2])
             
             # Create Poly3DCollection
@@ -279,9 +279,9 @@ class USpaceVisualizer:
                                    edgecolor='darkblue', linewidths=0.3)
             ax.add_collection3d(poly)
         
-        # Scatter with Grad-CAM coloring (swap axes: U3=x, U2=y, U1=z)
+        # Scatter with Grad-CAM coloring (swap axes: -U3=x, U2=y, U1=z)
         scatter = ax.scatter(
-            U_plot[:, 2], U_plot[:, 1], U_plot[:, 0],
+            -U_plot[:, 2], U_plot[:, 1], U_plot[:, 0],
             c=gradcam_plot,
             cmap='hot',
             s=2,
@@ -323,20 +323,20 @@ class USpaceVisualizer:
         fig = plt.figure(figsize=(14, 12))
         ax = fig.add_subplot(111, projection='3d')
         
-        # Plot hull again (swap axes: U3=x, U2=y, U1=z)
+        # Plot hull again (swap axes: -U3=x, U2=y, U1=z)
         if hull is not None:
             verts = []
             for simplex in hull.simplices:
-                v0 = [U_hull[simplex[0], 2], U_hull[simplex[0], 1], U_hull[simplex[0], 0]]
-                v1 = [U_hull[simplex[1], 2], U_hull[simplex[1], 1], U_hull[simplex[1], 0]]
-                v2 = [U_hull[simplex[2], 2], U_hull[simplex[2], 1], U_hull[simplex[2], 0]]
+                v0 = [-U_hull[simplex[0], 2], U_hull[simplex[0], 1], U_hull[simplex[0], 0]]
+                v1 = [-U_hull[simplex[1], 2], U_hull[simplex[1], 1], U_hull[simplex[1], 0]]
+                v2 = [-U_hull[simplex[2], 2], U_hull[simplex[2], 1], U_hull[simplex[2], 0]]
                 verts.append([v0, v1, v2])
             poly = Poly3DCollection(verts, alpha=0.15, facecolor='cyan', 
                                    edgecolor='darkblue', linewidths=0.3)
             ax.add_collection3d(poly)
         
         scatter = ax.scatter(
-            U_plot[:, 2], U_plot[:, 1], U_plot[:, 0],
+            -U_plot[:, 2], U_plot[:, 1], U_plot[:, 0],
             c=gradcam_plot,
             cmap='hot',
             s=2,
